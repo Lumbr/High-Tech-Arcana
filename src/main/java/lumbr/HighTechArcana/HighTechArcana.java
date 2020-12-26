22638872e6532f8ed5f9612efd4e9414b80619c3
Lumbr <jumiciobi@gmail.com>
2020-12-26 00:18:18 +0000

    added   H A M B O R G

--- src/main/java/lumbr/HighTechArcana/HighTechArcana.java
+++ src/main/java/lumbr/HighTechArcana/HighTechArcana.java
@@ -3,7 +3,13 @@ package lumbr.HighTechArcana;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import lumbr.HighTechArcana.lists.ItemList;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -33,5 +39,21 @@ public class HighTechArcana {
 	}
 	private void clientRegistries(final FMLClientSetupEvent event) {
 		logger.info("clientRegisteries method registered");
+	}
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents{
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event) {
+			event.getRegistry().registerAll(
+					ItemList.mcchicken = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("mcchicken"))
+			);
+			
+			logger.info("Items registered");
+		}
+		private static ResourceLocation location(String name) {
+			
+			return new ResourceLocation(modid, name);
+			
+		}
 	}
 }
---
+++ src/main/java/lumbr/HighTechArcana/lists/ItemList.java
@@ -0,0 +1,7 @@
+package lumbr.HighTechArcana.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+	public static Item mcchicken;
+}
