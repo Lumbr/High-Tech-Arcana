41081794228e121f8544aca3bfb888977e8cf0ce
Lumbr <jumiciobi@gmail.com>
2021-02-01 17:58:26 +0000

    half of armour but i had to go eat

---
+++ src/main/java/lumbr/HighTechArcana/lists/ArmorMaterialList.java
@@ -0,0 +1,78 @@
+package lumbr.HighTechArcana.lists;
+
+import lumbr.HighTechArcana.HighTechArcana;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+
+public enum ArmorMaterialList implements IArmorMaterial
+{
+	mcchicken("mcchicken", 400, new int[] {8,10,9,7}, 25, ItemList.mcchicken, "");
+	
+	public static final int[] max_damage_array = new int[] {13,15,16,11};
+	private String name, equipSound;
+	private int durability, enchantability;
+	private Item repairItem;
+	private int[] damageReductionAmounts;
+	private float toughness;
+	private void ArmourMaterialList(String name, int durability, int[] damageReductionAmounts, int enchantability, Item repairItem, String equipSound, float toughness) 
+	{
+		this.name = name;
+		this.equipSound = equipSound;
+		this.durability = durability;
+		this.enchantability = enchantability;
+		this.repairItem = repairItem;
+		this.damageReductionAmounts = damageReductionAmounts;
+		this.toughness = toughness;
+	}
+	@Override
+	public int getDurability(EquipmentSlotType slot)
+	{
+		// TODO Auto-generated method stub
+		return max_damage_array[slot.getIndex()] * this.durability;
+	}
+	@Override
+	public int getDamageReductionAmount(EquipmentSlotType slot)
+	{
+		return this.damageReductionAmounts[slot.getIndex()];
+	}
+	@Override
+	public int getEnchantability()
+	{
+		// TODO Auto-generated method stub
+		return this.enchantability;
+	}
+	@Override
+	public SoundEvent getSoundEvent()
+	{
+		// TODO Auto-generated method stub
+		return new SoundEvent(new ResourceLocation(equipSound));
+	}
+	@Override
+	public Ingredient getRepairMaterial()
+	{
+		// TODO Auto-generated method stub
+		return Ingredient.fromItems(this.repairItem);
+	}
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return HighTechArcana.modid + ":" + this.name;
+	}
+	@Override
+	public float getToughness()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public float getKnockbackResistance()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
--- src/main/java/lumbr/HighTechArcana/lists/ItemList.java
+++ src/main/java/lumbr/HighTechArcana/lists/ItemList.java
@@ -11,5 +11,11 @@ public class ItemList {
 	public static Item mcchicken_shovel;
 	public static Item mcchicken_sword;
 	
+	public static Item mcchicken_helmet;
+	public static Item mcchicken_chestplate;
+	public static Item mcchicken_leggings;
+	public static Item mcchicken_boots;
+	
+	
 	public static Item mcchicken_block;
 }
